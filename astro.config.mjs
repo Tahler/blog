48a3d6e22aec7648c903b6f8a57c964249613856
Tyler Berry <tyler@berryac.com>
2026-05-06 19:18:08 -0700

    Add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://bertyl.com',
+	adapter: vercel(),
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
