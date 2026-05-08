a5d2a0ea3eeb1f988f0907a8bd5451e07a84816e
Tyler Berry <tyler@berryac.com>
2026-05-07 21:57:06 -0700

    Move rss.xml.ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,17 +1,18 @@
 import rss from "@astrojs/rss";
-import MarkdownIt from 'markdown-it';
-import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from "markdown-it";
+import sanitizeHtml from "sanitize-html";
 import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 import { getSortedPosts } from "../lib/posts";
+import type { APIRoute } from "astro";
 
 const parser = new MarkdownIt();
 
-export async function GET(context) {
+export const GET: APIRoute = async ({ site }) => {
   const posts = await getSortedPosts();
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: context.site,
+    site: site || "",
     items: posts.map((post) => ({
       title: post.title,
       description: post.description,
@@ -19,8 +20,8 @@ export async function GET(context) {
       categories: [post.tag],
       link: post.url,
       content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
       }),
     })),
   });
-}
+};
